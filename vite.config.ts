dfaf3e20aa094b2e7969ddce615e852c3bb013f8
ADITYA NANDAN <adityanandan137@gmail.com>
2026-02-22 01:49:42 +0530

    feat: configure GitHub Pages deployment by adding a 404.html and updating the Vite base path.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 🟢 Added for GitHub Pages deployment (relative paths)
+  base: '/nutriscan-ai/', // 🟢 Required for GitHub Pages deployment
   server: {
     allowedHosts: ['acd0-2401-4900-1ca8-5f71-c8d8-7ef0-84d7-f874.ngrok-free.app']
   }
